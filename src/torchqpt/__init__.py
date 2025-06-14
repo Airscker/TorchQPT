88608e833cff576a887efd58f41a263878f1a333
airscker <airscker@gmail.com>
2025-06-13 23:41:24 -0400

    installation enabled

---
+++ src/torchqpt/__init__.py
@@ -0,0 +1,21 @@
+"""
+TorchQPT - A PyTorch-based library for quantum process tomography simulation and machine learning
+"""
+
+__version__ = "0.1.0"
+
+from .states import QuantumStateVector, DensityMatrix
+from .gates import *
+from .circuits import QuantumCircuit
+from .simulation import CircuitSimulator
+from .noise import *
+from .tomography import *
+from .tensor_network import MPS
+
+__all__ = [
+    'QuantumStateVector',
+    'DensityMatrix',
+    'QuantumCircuit',
+    'CircuitSimulator',
+    'MPS',
+] 
